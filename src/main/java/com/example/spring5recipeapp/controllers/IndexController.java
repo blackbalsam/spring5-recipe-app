c6ebdd890690477ba32069f4e1db0e2922d9ee63
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-05-16 16:19:25 +0300

    Added index page and index controller

---
+++ src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndex(Model model) {
+        System.out.println("Hello World!");
+        return "index";
+    }
+}
