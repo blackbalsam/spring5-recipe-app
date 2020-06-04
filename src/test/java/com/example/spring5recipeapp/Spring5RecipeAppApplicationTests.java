4b3cbe5c8d1c19ee07d39ed13d4a922238f7b929
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-06-04 18:37:50 +0300

    Added sanity check to see if application context starts

--- src/test/java/com/example/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/com/example/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.spring5recipeapp;
 
+import com.example.spring5recipeapp.controllers.IndexController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Spring5RecipeAppApplicationTests {
 
+	@Autowired
+	private IndexController indexController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(indexController).isNotNull();
 	}
 
 }
