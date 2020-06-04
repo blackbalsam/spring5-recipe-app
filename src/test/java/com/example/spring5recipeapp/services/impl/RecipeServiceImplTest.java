5d7063de4f10ad85d30679eedbf73f837fd5d114
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-06-04 18:24:35 +0300

    Added RecipeServiceImplTest

---
+++ src/test/java/com/example/spring5recipeapp/services/impl/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.example.spring5recipeapp.services.impl;
+
+import com.example.spring5recipeapp.domain.Recipe;
+import com.example.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1,recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
